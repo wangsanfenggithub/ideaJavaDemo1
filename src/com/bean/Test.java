3be401dbbfe597fab67d1a459a71cb351e603ad4
Administrator <690163977@qq.com>
2022-10-18 09:09:56 +0800

    版本三

--- src/com/bean/Test.java
+++ src/com/bean/Test.java
@@ -3,5 +3,6 @@ package com.bean;
 public class Test {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbbbb");
     }
 }
